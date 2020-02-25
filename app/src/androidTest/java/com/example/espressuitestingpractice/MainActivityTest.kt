e54c45f46e1e6bd790aa10829c72ee5359690d34
Aslmmon <Aslmmon993@gmail.com>
2020-02-25 15:01:00 +0200

    * Added Espresso Tests for Navigation between Activities

--- app/src/androidTest/java/com/example/espressuitestingpractice/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/espressuitestingpractice/MainActivityTest.kt
@@ -2,6 +2,8 @@ package com.example.espressuitestingpractice
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -15,11 +17,11 @@ import org.junit.runner.RunWith
 class MainActivityTest {
 
 
-
     @Test
     fun test_activityInView() {
 //        // this scenario will launch this specific Activity Isolated for this Test function
-      val senario =  ActivityScenario.launch(MainActivity::class.java)      //senario.moveToState(Lifecycle.State.CREATED)
+        val senario =
+            ActivityScenario.launch(MainActivity::class.java)      //senario.moveToState(Lifecycle.State.CREATED)
         onView(withId(R.id.main)).check(matches(isDisplayed()))
     }
 
@@ -36,5 +38,23 @@ class MainActivityTest {
     fun test_titleOfText_isDiplayedExactly() {
         ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.tv_main)).check(matches(withText(R.string.first_activity)))
+    }
+
+
+    @Test
+    fun test_nextBtnNavigateToSecondryActivity() {
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.btn_next)).perform(click())
+        onView(withId(R.id.second_activity)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_backButtonInSecondActivity_toNavigateToMainActivity() {
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.btn_next)).perform(click())
+        onView(withId(R.id.second_activity)).check(matches(isDisplayed()))
+        pressBack()
+        // or         onView(withId(R.id.btn_back)).perform(click))
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
     }
 }
