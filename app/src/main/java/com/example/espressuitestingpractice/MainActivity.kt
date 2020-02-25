882cb45e6cc4f645e6264d47e3a9d04152942be6
Aslmmon <Aslmmon993@gmail.com>
2020-02-25 15:23:58 +0200

    * Added activity layout in Constructor of AppCompat

--- app/src/main/java/com/example/espressuitestingpractice/MainActivity.kt
+++ app/src/main/java/com/example/espressuitestingpractice/MainActivity.kt
@@ -5,11 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         btn_next.setOnClickListener {
             startActivity(Intent(this,SecondActivity::class.java))
         }
