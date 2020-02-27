de9497f19e9e3942267920a35067b864638fd91f
Aslmmon <Aslmmon993@gmail.com>
2020-02-27 15:18:10 +0200

    * Added All Test Case in Test Suites

--- app/src/androidTest/java/com/example/espressuitestingpractice/ActivityTestSuite.kt
+++ app/src/androidTest/java/com/example/espressuitestingpractice/ActivityTestSuite.kt
@@ -1,11 +1,15 @@
 package com.example.espressuitestingpractice
 
+import com.example.espressuitestingpractice.fragments.FirstFragmentTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
     MainActivityTest::class,
+    SecondActivityTest::class,
+    NavigationTest::class,
+    FirstFragmentTest::class,
     SecondActivityTest::class
 )
 class ActivityTestSuite
